826a1fe3e0f619fd8f42d69ddf98bf0c65276923
Alexander “weej” Jones <alex@weej.com>
2023-04-09 21:14:29 +0100

    Demo issue

--- App.tsx
+++ App.tsx
@@ -31,13 +31,30 @@ const data = Array.from({length: 500}, (_, i) => ({
   text: `This is another item with index ${i}`,
 }));
 
+type Item = (typeof data)[0];
+
+const ITEM_HEIGHT = 20;
+
 function App() {
-  const renderItem = ({item}: {item: (typeof data)[0]}) => {
+  const renderItem = ({item}: {item: Item}) => {
     console.info('rendering FlatList item', item.id, new Date());
     return <MyComponent text={item.text} id={item.id} />;
   };
 
-  return <FlatList data={data} renderItem={renderItem} />;
+  const getItemLayout = (data: Item[] | null | undefined, index: number) => ({
+    length: ITEM_HEIGHT,
+    offset: ITEM_HEIGHT * index,
+    index,
+  });
+
+  return (
+    <FlatList
+      data={data}
+      renderItem={renderItem}
+      keyExtractor={item => `${item.id}`}
+      getItemLayout={getItemLayout}
+    />
+  );
 }
 
 const MyComponent = React.memo(function MyComponent(props: {
@@ -46,7 +63,7 @@ const MyComponent = React.memo(function MyComponent(props: {
 }) {
   console.info('rendering MyComponent', props.id, new Date());
   return (
-    <View>
+    <View style={{height: ITEM_HEIGHT}}>
       <Text>{props.text}</Text>
     </View>
   );
